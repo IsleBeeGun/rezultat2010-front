b34afd2ce5380e81ddf4ea39d547bd208aa374a1
IsleBeeGun <IsleBeeGun@gmail.com>
2021-02-08 03:15:15 +0300

    initial commit

---
+++ src/js/bundle.js
@@ -0,0 +1,43 @@
+// Setup Swipper
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+Swiper.use([Navigation, Pagination]);
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 1,
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  simulateTouch: false,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 530px
+    530: {
+      slidesPerView: 3,
+      spaceBetween: 1,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// Setup zenScroll
+import zenScroll from 'zenscroll';
+zenScroll.setup(null, 0); // setting top offset to zero
+
+// Setup styles
+import '../styles/bundle.scss';
+
+// Setup modals
+import './modal';
--- src/js/modal.js
+++ src/js/modal.js
@@ -8,10 +8,21 @@ const closeModal = () => {
   document.querySelector('html').style.overflow = 'visible';
 };
 
-modalExit.addEventListener('click', event => {
+modalExit.addEventListener('click', (event) => {
   closeModal();
 });
 
-modalBackdrop.addEventListener('click', event => {
+modalBackdrop.addEventListener('click', (event) => {
   closeModal();
 });
+
+globalThis.openModal = (msg) => {
+  document.querySelector('.modal-message').innerHTML = msg;
+  document.querySelector('html').style.overflow = 'hidden';
+  document
+    .querySelector('.modal-backdrop')
+    .setAttribute('style', 'visibility:visible; opacity: 1;');
+  document
+    .querySelector('.modal-window')
+    .setAttribute('style', 'visibility:visible; opacity: 1;');
+};
